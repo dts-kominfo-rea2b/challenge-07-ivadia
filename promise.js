eefeaabe1ebe2c029ddc0c03d7a15dfb5afea05d
Ivadia Elmina Patola <ivadia2401@gmail.com>
2022-06-10 12:34:54 +0800

    done challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,10 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const result = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]).then(res => res[0].concat(res[1]));
+  return result.filter(item => item.hasil === emosi).length;
+};
 
 module.exports = {
   promiseOutput,
